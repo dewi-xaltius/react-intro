5a248a585eb7a2df0305eebd5bbc06f4e6b528c6
dewi-xaltius <smdewi@xaltius.tech>
2024-09-29 11:03:10 +0800

    Initial commit, MyComponent.js

---
+++ src/components/MyComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <h2>Develop First React Component</h2>
+        <p>This is my custom component.</p>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
