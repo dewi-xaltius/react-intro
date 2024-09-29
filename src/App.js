ebb5c2918be2010aa990a6bd5abb034d18c6bb83
dewi-xaltius <smdewi@xaltius.tech>
2024-09-29 12:23:04 +0800

    add functional component MyFunctionalComponent.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
+import MyFunctionalComponent from './components/MyFunctionalComponent';
 
 function App() {
   return (
     <div className="App">
       <h1>My React App</h1>
       <MyComponent />
+      <MyFunctionalComponent />
 
     </div>
   );
---
+++ src/components/MyFunctionalComponent.js
@@ -0,0 +1,11 @@
+const MyFunctionalComponent = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <h2>First Functional React Component</h2>
+      <p>This is my custom functional component.</p>
+    </div>
+  );
+};
+
+export default MyFunctionalComponent;
