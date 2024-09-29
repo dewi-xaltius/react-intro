76bfee06702fa2c65351c3a62b976681b0971d66
dewi-xaltius <smdewi@xaltius.tech>
2024-09-29 17:27:08 +0800

    add DisplayData and DisplayList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Button from './components/Button';
 import ButtonStyled from './components/ButtonStyled';
+import DisplayData from './components/DisplayData';
+import DisplayList from './components/DisplayList';
 import InlineStyledDiv from './components/InlineStyle';
 import MyComponent from './components/MyComponent';
 import MyFunctionalComponent from './components/MyFunctionalComponent';
@@ -16,6 +18,8 @@ function App() {
       <InlineStyledDiv />
       <Button />
       <ButtonStyled />
+      <DisplayData />
+      <DisplayList />
     </div>
   );
 }
---
+++ src/components/DisplayData.js
@@ -0,0 +1,6 @@
+const DisplayData = () => {
+    const name = "John";
+    return <h1>Hello, {name}!</h1>;
+  };
+
+export default DisplayData;
